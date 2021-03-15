20989c3ab8399631754bc87e81e97d049ca8ecba
veeteeran <vietnamtran@gmail.com>
2021-03-15 20:25:19 +0000

    Change return statement

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,4 +5,4 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return a Callable, multiples multiplier by a float"""
-    return lambda x: multiplier * 2.0
+    return lambda x: multiplier * multiplier
