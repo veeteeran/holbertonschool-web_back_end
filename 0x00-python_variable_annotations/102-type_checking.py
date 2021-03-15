21a2ae5ca5e81f269b8902d24981ae0b797201b6
veeteeran <vietnamtran@gmail.com>
2021-03-15 22:59:50 +0000

    Add task 12, not correct

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""Docstring for task 12"""
+import math
+from typing import Any, List, Tuple
+
+
+def zoom_array(lst: List[int], factor: float = 2) -> List:
+    factor = math.floor(factor)
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
