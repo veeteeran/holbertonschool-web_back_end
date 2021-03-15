44764edeb3cc71158b32f1c76fd0854856de5335
veeteeran <vietnamtran@gmail.com>
2021-03-15 16:07:13 +0000

    Add task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""This is a docstring for task 0"""
+
+
+def add(a: float, b: float) -> float:
+    """Type annotated function. Sums two floats. Returns a float""" 
+
+    return a + b
