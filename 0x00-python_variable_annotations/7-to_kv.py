786cfa0ab000a756bb1835c2169b18b56bc86e5a
veeteeran <vietnamtran@gmail.com>
2021-03-15 19:43:58 +0000

    Add task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Docstring for task 7"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple. Values of tuple are k, v squared"""
+    return (k, v * v)
