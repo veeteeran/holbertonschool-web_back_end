d7a25eff9c753960a6e58e99786ea23c4c734d4b
veeteeran <vietnamtran@gmail.com>
2021-03-15 19:04:52 +0000

    Delete trailing whitespace

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,6 +3,6 @@
 
 
 def add(a: float, b: float) -> float:
-    """Type annotated function. Sums two floats. Returns a float""" 
+    """Type annotated function. Sums two floats. Returns a float"""
 
     return a + b
