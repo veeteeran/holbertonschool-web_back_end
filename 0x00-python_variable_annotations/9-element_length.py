59552d2e98163540cc4b4076c534fa3fe82984ff
veeteeran <vietnamtran@gmail.com>
2021-03-15 21:15:59 +0000

    Add task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Docstring for task 9"""
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Not sure I understand the point of this"""
+    return [(i, len(i)) for i in lst]
