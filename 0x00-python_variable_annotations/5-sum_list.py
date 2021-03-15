4a4931e529c80440abb4d126723eb80abcb6a583
veeteeran <vietnamtran@gmail.com>
2021-03-15 18:58:56 +0000

    Add task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Docstring for task 5"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return sum of floats in input_list"""
+    return sum(input_list)
