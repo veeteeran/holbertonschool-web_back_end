68aecfa6e42dcf1b31d636761ff3ace962ff2cee
veeteeran <vietnamtran@gmail.com>
2021-03-15 20:22:08 +0000

    Add task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Docstring for task 8"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a Callable, multiples multiplier by a float"""
+    return lambda x: multiplier * 2.0
