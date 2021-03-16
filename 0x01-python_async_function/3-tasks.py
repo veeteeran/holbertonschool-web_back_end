0b87a45b2f02e98ce230092b91e41248e1f647cd
veeteeran <vietnamtran@gmail.com>
2021-03-16 21:01:11 +0000

    Fix typo

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,7 +4,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asynchio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
         Returns a asyncio.Task
 
