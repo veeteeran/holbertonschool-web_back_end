a9e8c648b45ac5a1df117ba29077ea6e6c36b6af
veeteeran <vietnamtran@gmail.com>
2021-03-16 20:54:08 +0000

    Delete trailing whitespace

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -14,4 +14,4 @@ def task_wait_random(max_delay: int) -> asynchio.Task:
                 Returns:
                         asyncio.Task
     """
-    return asyncio.create_task(wait_random(max_delay)) 
+    return asyncio.create_task(wait_random(max_delay))
