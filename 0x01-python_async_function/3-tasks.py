20185410083738a73c8c441b373ce8b245a4a011
veeteeran <vietnamtran@gmail.com>
2021-03-16 20:39:49 +0000

    Add docstring

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,4 +6,13 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> Callable:
+    """
+        Returns a asyncio.Task
+
+                Parameter:
+                       max_delay (int): max delay time passed to wait_random
+
+                Returns:
+                        asyncio.Task
+    """
     return asyncio.create_task(wait_random(max_delay))
