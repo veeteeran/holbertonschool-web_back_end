15f2ddcc8876d48f8962116a42538f33a7a58bed
veeteeran <vietnamtran@gmail.com>
2021-03-16 20:35:11 +0000

    Add task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Docstring for task 3"""
+import asyncio
+from typing import Callable
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Callable:
+    return asyncio.create_task(wait_random(max_delay))
