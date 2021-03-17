27b8e7d007141683aba53ee5943e4dc41abc8fa6
veeteeran <vietnamtran@gmail.com>
2021-03-17 15:16:16 +0000

    Return instead of yield to make async a coroutine

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,6 +8,5 @@ from typing import Generator
 async def async_generator() -> Generator[int, None, None]:
     """Yield a random number between 0 and 10"""
     for i in range(10):
-        # random_val = uniform(0, 10)
         await asyncio.sleep(1)
-        yield uniform(0, 10)
+        return uniform(0, 10)
