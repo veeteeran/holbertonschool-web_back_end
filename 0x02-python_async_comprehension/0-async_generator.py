487c2e387607273c749098c7c80903c76bff53dc
veeteeran <vietnamtran@gmail.com>
2021-03-17 14:47:16 +0000

    Add task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Docstring for task 0"""
+import asyncio
+from random import uniform
+from typing import Generator
+
+
+async def async_generator() -> Generator[int, None, None]:
+    """Yield a random number between 0 and 10"""
+    for i in range(10):
+        # random_val = uniform(0, 10)
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
