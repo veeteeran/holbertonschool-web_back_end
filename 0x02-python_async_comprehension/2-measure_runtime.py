20627fc1cdbd75c43a813582fc629e72b4678f34
veeteeran <vietnamtran@gmail.com>
2021-03-17 17:37:33 +0000

    Change formatting

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -13,12 +13,8 @@ async def measure_runtime() -> float:
     """
     start = time.time()
 
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
 
     end = time.time()
 
