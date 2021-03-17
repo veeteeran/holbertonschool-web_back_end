e09e877d75f6087e0e96c3428c4891a4025f9053
veeteeran <vietnamtran@gmail.com>
2021-03-17 17:26:01 +0000

    Use Generator as return type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Docstring for task 0"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield a random number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
