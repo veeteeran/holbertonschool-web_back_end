4463ec4ac540268b34fefd82c16f013261603d42
veeteeran <vietnamtran@gmail.com>
2021-03-23 15:41:30 +0000

    Add task 0

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""Index range module"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+        Return tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters
+
+            Parameters:
+                    page(int): page number
+                    page_size(int): items on each page
+
+            Return:
+                    A tuple of the start index and end index
+    """
+    start = (page * page_size) - page_size
+    end = page * page_size
+
+    return (start, end)
