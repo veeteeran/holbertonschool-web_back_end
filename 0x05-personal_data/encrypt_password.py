779d9a33eb0807d465013b865eb87a03e1a7925d
veeteeran <vietnamtran@gmail.com>
2021-03-29 12:47:56 +0000

    Add task 5

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Hash password module"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+        Returns a salted, hashed password as a byte string
+
+            Parameter:
+                    password(str): the password to hash
+    """
+    password = bytes(password, 'utf-8')
+    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    print(type(hashed))
+    return hashed
