9db0e4eac868a6cd4a1bbc5f7cbfbfa71397b17f
veeteeran <vietnamtran@gmail.com>
2021-03-30 22:19:19 +0000

    Add check for list is None in require_auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -10,18 +10,25 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require path method"""
-        if path is not None and path[-1] != '/':
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path[-1] != '/':
             path = path + '/'
 
-        if path is None or excluded_paths is [] or path not in excluded_paths:
+        if path not in excluded_paths:
             return True
 
         return False
 
     def authorization_header(self, request=None) -> str:
         """authorization_header method"""
-        return request
+        print(f"Authorization: {request.headers.get('Authorization')}")
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """current_user method"""
-        return request
+        return None
