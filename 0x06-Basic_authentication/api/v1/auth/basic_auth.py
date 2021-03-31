a2adb614d97fac66e0ebc0c3725e36d2eac5b8a7
veeteeran <vietnamtran@gmail.com>
2021-03-31 02:26:35 +0000

    Add Base64 decode

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic Auth Class module"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,22 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+            Returns the decoded value of a Base64 string
+            base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            return b64decode(base64_authorization_header,
+                             None, True).decode('utf-8')
+        except Exception:
+            return None
