dc4c1a9496200bed86ed2e87faa2baee50c906c0
veeteeran <vietnamtran@gmail.com>
2021-03-31 02:10:10 +0000

    Add extract method

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,19 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic Auth inherits from Auth"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+            Returns the Base64 part of the Authorization
+            header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+
+        if type(authorization_header) is not str:
+            return None
+
+        if authorization_header[0:6] != 'Basic ':
+            return None
+
+        return authorization_header[6:]
