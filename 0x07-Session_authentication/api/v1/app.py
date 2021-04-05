c1ea7217851b5d9d7be0c4fd7851762b7255167a
veeteeran <vietnamtran@gmail.com>
2021-04-05 19:27:42 +0000

    Update @app.before_request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -50,10 +50,12 @@ def before_request_handler():
     """before_request handler"""
     excluded = ['/api/v1/status/',
                 '/api/v1/unauthorized/',
-                '/api/v1/forbidden/']
+                '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/']
 
     if auth is not None and auth.require_auth(request.path, excluded):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and\
+                auth.session_cookie(request):
             abort(401)
 
         if auth.current_user(request) is None:
