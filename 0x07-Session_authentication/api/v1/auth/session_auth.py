53f3e27a6f48ed6be2e51a9a4cc351e72461b38b
veeteeran <vietnamtran@gmail.com>
2021-04-05 17:31:29 +0000

    Add create_session method to SessionAuth

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 """Session Auth Class module"""
 from api.v1.auth.auth import Auth
-from base64 import b64decode, b64encode
-import hashlib
-from models.base import DATA
-from models.user import User
-from typing import Tuple, TypeVar
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session Auth inherits from Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id.update({session_id: user_id})
+
+        return session_id
