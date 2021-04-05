14626754140f27be4416f681fd533ba7d628206d
veeteeran <vietnamtran@gmail.com>
2021-04-05 19:34:34 +0000

    Fix condition statement in @app.before_request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -55,7 +55,7 @@ def before_request_handler():
 
     if auth is not None and auth.require_auth(request.path, excluded):
         if auth.authorization_header(request) is None and\
-                auth.session_cookie(request):
+                auth.session_cookie(request) is None:
             abort(401)
 
         if auth.current_user(request) is None:
