e2da023b0a698d63161d0d1584287f66984b716e
veeteeran <vietnamtran@gmail.com>
2021-04-06 17:06:04 +0000

    Add view to handle all routes for sesh auth

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Module of Session Auth views
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_auth():
+    """Handles all routes for the Session authentication"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or email == '':
+        return jsonify({ "error": "email missing" }), 400
+
+    if password is None or password == '':
+        return jsonify({ "error": "password missing" }), 400
+
+    users = User.search({'email': email})
+    if users == []:
+        return jsonify({ "error": "no user found for this email" }), 404
+
+    for user in users:
+        if user.email == email and not user.is_valid_password(password):
+            return jsonify({ "error": "wrong password" }), 401
+        else:
+            from api.v1.app import auth
+            session_name = getenv('SESSION_NAME')
+            session_id = auth.create_session(user.id)
+            out = jsonify(state=0, msg='success')
+            out.set_cookie(session_name, session_id)
+            return out
