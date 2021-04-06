8c04bc419b2880320734c4f424221898e44591c9
veeteeran <vietnamtran@gmail.com>
2021-04-06 18:37:36 +0000

    Update SessionAuth and add logout route

--- 0x07-Session_authentication/check.sh
+++ 0x07-Session_authentication/check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pycodestyle "api/v1/app.py" "api/v1/auth/auth.py" "api/v1/auth/basic_auth.py"
+pycodestyle "api/v1/app.py" "api/v1/auth/auth.py" "api/v1/auth/basic_auth.py" "api/v1/views/session_auth.py" "api/v1/auth/session_auth.py"
 
 #python3 -c 'print(__import__("api/v1/app").__doc__)'
 #python3 -c 'print(__import__("api/v1/auth/auth.py").Auth.__doc__)'
