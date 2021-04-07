ee7d034add4412997ec8415b6944a449681457f2
veeteeran <vietnamtran@gmail.com>
2021-04-07 16:15:35 +0000

    Use user_id_for_session_id in overload

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -23,9 +23,10 @@ class SessionDBAuth(SessionExpAuth):
 
     def user_id_for_session_id(self, session_id=None):
         """Returns a User ID based on a Session ID"""
-        session_dict = self.user_id_by_session_id.get(session_id)
-        return session_dict.get('user_id')
-        # return super().user_id_for_session_id()
+        #session_dict = self.user_id_by_session_id.get(session_id)
+        #return session_dict.get('user_id')
+        user_id = super().user_id_for_session_id(session_id)
+        return user_id
 
     def destroy_session(self, request=None):
         """Deletes the user session / logout"""
