9ce96ee322c1e3fc99707a8e5b5b8162d46cf739
veeteeran <vietnamtran@gmail.com>
2021-04-08 22:20:14 +0000

    Add _hash_password function

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Auth module"""
+import bcrypt
+
+
+def _hash_password(password: str) -> str:
+    """Return a salted hash of the input password"""
+    hashed = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+
+    return str(hashed)
