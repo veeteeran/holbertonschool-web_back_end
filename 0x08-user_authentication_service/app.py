2f5761fc9e8024421a10d03ba339b23e91f62926
veeteeran <vietnamtran@gmail.com>
2021-04-09 23:00:40 +0000

    Add logout route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Basic Flask app
 """
 from auth import Auth
-from flask import Flask, abort, jsonify, request
+from flask import Flask, abort, jsonify, redirect, request, url_for
 
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
-def home():
+def index():
     """ GET /
     Return:
     - jsonifiy {"message": "Bienvenue"}
@@ -38,7 +38,7 @@ def users():
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login():
-    """ POST /seesions/
+    """ POST /sessions/
     Create new session, store session ID as cookie, key is session_id on the\
             response
     Return:
@@ -55,6 +55,21 @@ def login():
         return response
     else:
         abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Logout and delete current session"""
+    session_id = request.cookie.get('session_id')
+    if session_id is None:
+        return None
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect(url_for('index'))
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
