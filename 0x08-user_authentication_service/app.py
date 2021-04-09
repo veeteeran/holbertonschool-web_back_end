29be82b805a9b0f3f972cc0e2ff9cfb133468faa
veeteeran <vietnamtran@gmail.com>
2021-04-09 20:22:53 +0000

    Add login function to sessions route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Basic Flask app
 """
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 
 
 app = Flask(__name__)
@@ -34,6 +34,27 @@ def users():
         return jsonify(email=email, message="user created")
     except ValueError:
         return jsonify(message="email already registered"), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /seesions/
+    Create new session, store session ID as cookie, key is session_id on the\
+            response
+    Return:
+        - JSON payload of the form
+        - 401 if login information is incorrect
+    """
+    email = request.form['email']
+    password = request.form['password']
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify(email=email, message="logged in")
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
