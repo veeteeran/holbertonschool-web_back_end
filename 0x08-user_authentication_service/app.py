f615319b23f8ee20d1fde03bcd210e1b067f8338
veeteeran <vietnamtran@gmail.com>
2021-04-10 16:34:25 +0000

    Add reset_password route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -89,5 +89,23 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """ POST /reset_password
+    Generates a token
+    Return:
+        - Status code 200
+        - payload {"email": "<user email>", "reset_token": "<reset token>"}
+        - 403 if email not registered
+    """
+    email = request.args.get('email')
+    if email:
+        try:
+            token = AUTH.get_reset_password_token(email)
+            return jsonify(email=email, reset_token=token), 200
+        except ValueError:
+            abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
