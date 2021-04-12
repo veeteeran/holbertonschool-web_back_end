985a9711c2e85c5fe4f2de00fcc997c5c34180f1
veeteeran <vietnamtran@gmail.com>
2021-04-12 14:30:36 +0000

    Add docstring delete extra import

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+"""DB class module"""
 from sqlalchemy import create_engine
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
