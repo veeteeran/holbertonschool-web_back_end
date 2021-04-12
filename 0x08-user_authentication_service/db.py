7476dd36b98b728bb05b7327250f4fb6f94c4c44
veeteeran <vietnamtran@gmail.com>
2021-04-12 14:34:08 +0000

    Add annotation

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
 
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """
            Returns the first row found in the users table as filtered
            by the method’s input arguments
