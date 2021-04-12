65a14fab44a22b4d032cdf1ce6d3e187f177eda3
Viet Tran <vietnamtran@gmail.com>
2021-04-11 19:20:57 -0500

    Add echo=True back

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -13,7 +13,7 @@ class DB:
     """DB Class creates a DB object"""
     def __init__(self):
         """Initializes a DB object"""
-        self._engine = create_engine("sqlite:///a.db")
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
