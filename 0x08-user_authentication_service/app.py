4d246dfc5b3e24906bcc9f9a80974d677b912f80
veeteeran <vietnamtran@gmail.com>
2021-04-12 19:52:45 +0000

    Delete status return

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -123,7 +123,7 @@ def update_password():
     password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify(email=email, message="Password updated"), 200
+        return jsonify(email=email, message="Password updated")
     except Exception:
         abort(403)
 
