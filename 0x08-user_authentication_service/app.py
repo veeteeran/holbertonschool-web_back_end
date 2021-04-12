73951be001deef2f7df1c35739f4e5b6773aad30
veeteeran <vietnamtran@gmail.com>
2021-04-12 15:13:36 +0000

    Write condition differently

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -31,7 +31,8 @@ def users():
     email = request.form.get('email')
     password = request.form.get('password')
     try:
-        if AUTH.register_user(email, password):
+        user = AUTH.register_user(email, password):
+        if user:
             return jsonify(email=email, message="user created")
     except ValueError:
         return jsonify(message="email already registered"), 400
