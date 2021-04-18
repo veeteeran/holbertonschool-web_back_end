04f369a74fae971edfe5a36aa51be4b68f0e3178
veeteeran <vietnamtran@gmail.com>
2021-04-18 22:08:56 +0000

    Cleanup test_org

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -19,14 +19,7 @@ class TestGitHubOrgClient(unittest.TestCase):
     def test_org(self, org_name, mock_get_json):
         """Test that GitHubOrgClient.org returns the correct value"""
         test_obj = GitHubOrgClient(org_name)
-        arg = test_obj.ORG_URL.format(org=org_name)
         url = f"https://api.github.com/orgs/{org_name}"
-        self.assertEqual(arg, url)
-        '''
-        mock_get_json.return_value = {'key': 'value'}
-        response = test_obj.org()
-        self.assertEqual(response, {'key': 'value'})
-        '''
         test_obj.org()
         mock_get_json.assert_called_once_with(url)
 
