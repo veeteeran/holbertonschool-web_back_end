a9a2a7f0d30e6453b7ff5c7033dcf7de356c600b
veeteeran <vietnamtran@gmail.com>
2021-04-21 15:36:30 +0000

    Add Config class

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3
+"""Module of routes for task 0x0A"""
+from flask import Flask, render_templatei, requests
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.config = Config()
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Template for 0-index with title and header"""
+    return render_template('0-index.html',
+                           title='Welcome to Holberton',
+                           header='Hello world')
+
+class Config(object):
+    """Config class to setup Babel for English and French"""
+    LANGUAGES = ["en", "fr"]
+    babel.default_locale = "en"
+    babel.default_timezone = "UTC"
+
+    '''
+    @babel.localeselector
+    def default_locale(self):
+        """Config default locale to en"""
+        return app.config['BABEL_DEFAULT_LOCALE'] = "en"
+
+    @babel.timezoneselector
+    def default_timezone(self):
+        """Config default locale to UTC"""
+        return app.config['BABEL_DEFAULT_TIMEZONE'] = "UTC"
+    '''
