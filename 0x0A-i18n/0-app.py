a6e34d8576e53ec29a3e9b1d6707cf57719eaf17
veeteeran <vietnamtran@gmail.com>
2021-04-21 14:42:07 +0000

    Add doctrings

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Module of routes for task 0x0A"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Template for 0-index with title and header"""
     return render_template('0-index.html',
                            title='Welcome to Holberton',
                            header='Hello world')
