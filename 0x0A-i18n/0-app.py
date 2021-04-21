c93f56d7c08f1f1893066fb83e93b0d1025e5c9b
veeteeran <vietnamtran@gmail.com>
2021-04-21 14:18:33 +0000

    Add task 0

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('0-index.html',
+                           title='Welcome to Holberton',
+                           header='Hello world')
