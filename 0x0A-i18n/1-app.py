6065230fad9af41718eb3bf497c35afda1353ad7
veeteeran <vietnamtran@gmail.com>
2021-04-21 15:41:30 +0000

    Add get_locale method

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module of routes for task 0x0A"""
-from flask import Flask, render_templatei, requests
+from flask import Flask, render_template, requests
 from flask_babel import Babel
 
 app = Flask(__name__)
--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3
+"""Module of routes for task 0x0A"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.config = Config()
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Template for 0-index with title and header"""
+    return render_template('0-index.html',
+                           title='Welcome to Holberton',
+                           header='Hello world')
+
+class Config(object):
+    """Config class to setup Babel for English and French"""
+    LANGUAGES = ["en", "fr"]
+    babel.default_locale = "en"
+    babel.default_timezone = "UTC"
+
+    @babel.localeselector
+    def get_locale(self):
+        """Get user locale"""
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+    '''
+    @babel.timezoneselector
+    def default_timezone(self):
+        """Config default locale to UTC"""
+        return app.config['BABEL_DEFAULT_TIMEZONE'] = "UTC"
+    '''
