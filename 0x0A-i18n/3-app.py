c2d4f17d3b396f4ce7825033f8c50c4f6d08450a
veeteeran <vietnamtran@gmail.com>
2021-04-21 16:14:40 +0000

    Change index file number

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+"""Module of routes for task 0x0A"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from gettext import gettext
+
+app = Flask(__name__)
+app.config = Config()
+babel = Babel(app)
+_ = gettext
+
+
+@app.route('/')
+def index():
+    """Template for 3-index with title and header"""
+    return render_template('3-index.html',
+                           title=_('Welcome to Holberton'),
+                           header=_('Hello world'))
+
+class Config(object):
+    """Config class to setup Babel for English and French"""
+    LANGUAGES = ["en", "fr"]
+    babel.default_locale = "en"
+    babel.default_timezone = "UTC"
+
+    @babel.localeselector
+    def get_locale(self):
+        """Get user locale"""
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+    '''
+    @babel.timezoneselector
+    def default_timezone(self):
+        """Config default locale to UTC"""
+        return app.config['BABEL_DEFAULT_TIMEZONE'] = "UTC"
+    '''
