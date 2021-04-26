1956af52c51e6aee290b2cbb38e73294c57d1123
veeteeran <vietnamtran@gmail.com>
2021-04-26 15:55:15 +0000

    Add Cache class and store method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""Redis tasks"""
+import redis
+from typing import Union
+from uuid import uuid4
+
+
+class Cache():
+    """Redis cache object"""
+    def __init__(self):
+        """Initialize a Cache object"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[bytes, float, int, str]) -> str:
+        """Generate random uuid key, store data in key, return key"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
