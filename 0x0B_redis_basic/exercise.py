662aad35f53d6f7b02ecde2577d71ba97a9c656d
veeteeran <vietnamtran@gmail.com>
2021-04-27 16:28:40 +0000

    Add count_call wrapper

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 """Redis Cache module"""
+from functools import wraps
 import redis
 from typing import Callable, Optional, Union
 from uuid import uuid4
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count number of times methods are called"""
+    @wraps(method)
+    def count_calls_wrapper(self, *args):
+        """Counts number of calls wrapped function makes"""
+        self._redis.incr(count_calls_wrapper.__qualname__)
+        return method(self, *args)
+
+    return count_calls_wrapper
 
 
 class Cache():
@@ -12,6 +24,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate random uuid key, store data in key, return key"""
         key = str(uuid4())
