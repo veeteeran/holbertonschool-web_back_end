dbbca62c9e473ccd97f85ddebb74ef8f1fc188c7
veeteeran <vietnamtran@gmail.com>
2021-04-27 16:19:05 +0000

    Add count_calls decorator

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -9,28 +9,12 @@ from uuid import uuid4
 def count_calls(method: Callable) -> Callable:
     """Decorator to count number of times methods are called"""
     @wraps(method)
-    def count_calls_wrapper(self, method):
+    def count_calls_wrapper(self, *args):
         """Counts number of calls wrapped function makes"""
         self._redis.incr(count_calls_wrapper.__qualname__)
+        return method(*args)
 
     return count_calls_wrapper
-
-
-def call_history(method):
-    """Decorator to store the history of inputs and outputs for a function"""
-    inputs = method.__qualname__ + ":inputs"
-    outputs = method.__qualname__ + ":outputs"
-
-    @wraps(method)
-    def call_history_wrapper(self, args):
-        """Stores the history of inputs and outputs for a function"""
-        self._redis.rpush(inputs, str(args))
-        out = method(self, args)
-        self._redis.rpush(outputs, out)
-
-        return out
-
-    return call_history_wrapper
 
 
 class Cache():
@@ -40,7 +24,6 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate random uuid key, store data in key, return key"""
@@ -48,10 +31,19 @@ class Cache():
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None):
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """get data based on key, return data as specified type based on fn"""
         if fn:
             value = self._redis.get(key)
             return fn(value)
 
         return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """Typecast the key to a str"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Typecast the key to an int"""
+        return self.get(key, int)
