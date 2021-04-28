c393d70ae2b846308df601649ed9f3f3612d2785
veeteeran <vietnamtran@gmail.com>
2021-04-28 14:03:19 +0000

    Delete create db

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,5 @@
 -- SQL script that creates a table users
 -- Attributes id, email, name
-CREATE DATABASE IF NOT EXISTS holberton;
-USE holberton;
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	email VARCHAR(256) NOT NULL UNIQUE,
