893806b42deeda79703cbb035c5fa29cefc55b41
veeteeran <vietnamtran@gmail.com>
2021-04-29 15:49:44 +0000

    Specify which user_id to assign

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -6,7 +6,7 @@ BEGIN
 	SET @average = (
 		SELECT AVG(score) 
 		FROM corrections 
-		WHERE user_id = user_id);
+		WHERE corrections.user_id = user_id);
 
 	UPDATE users
 	SET average_score = @average
