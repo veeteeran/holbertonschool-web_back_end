38d867ff9ac116b9ac0543279f386f477c5106bb
veeteeran <vietnamtran@gmail.com>
2021-05-03 11:31:12 -0700

    Add insert_school function

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
-def list_all(mongo_collection):
+"""insert_school function module"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection based on kwargs"""
+    return mongo_collection.insert(kwargs)
+
+
+if __name__ == "__main__":
+    insert_school(mongo_collection, **kwargs)
