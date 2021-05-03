1c20a3fd8cbcf9d573497718adeec1ad53594f0a
veeteeran <vietnamtran@gmail.com>
2021-05-03 11:52:21 -0700

    Update main guard

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ def update_topics(mongo_collection, name, topics):
 
 
 if __name__ == "__main__":
-    insert_school(mongo_collection, name, topics)
+    update_topics(mongo_collection, name, topics)
