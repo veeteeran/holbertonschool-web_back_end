c49a17a43ac21f2d4b4e75c9758103675af92fec
veeteeran <vietnamtran@gmail.com>
2021-05-03 09:37:15 -0700

    Add list_all function

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
+"""list_all function module"""
+
+
 def list_all(mongo_collection):
+    """Lists all documents in a collection"""
+    return mongo_collection.find() or []
+
+
+if __name__ == "__main__":
+    list_all(mongo_collection)
