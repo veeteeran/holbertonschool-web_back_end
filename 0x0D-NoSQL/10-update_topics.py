8a73ef4d55a8bd149b29d49e89e3219fac24ab0c
veeteeran <vietnamtran@gmail.com>
2021-05-03 11:47:13 -0700

    Add update_topics function

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
-def list_all(mongo_collection):
+"""update_topics function module"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document based on the name"""
+    mongo_collection.update({'name': name}, {'$set': {'topics': topics}})
+
+
+if __name__ == "__main__":
+    insert_school(mongo_collection, name, topics)
