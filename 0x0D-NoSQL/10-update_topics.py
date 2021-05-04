b462bd5a7338df8e94d7d57e88127b51fd09d418
veeteeran <vietnamtran@gmail.com>
2021-05-04 09:20:41 -0700

    Fix typo

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -4,7 +4,7 @@
 
 def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on the name"""
-    mongo_collection.updateMany({'name': name}, {'$set': {'topics': topics}})
+    mongo_collection.update_many({'name': name}, {'$set': {'topics': topics}})
 
 
 if __name__ == "__main__":
