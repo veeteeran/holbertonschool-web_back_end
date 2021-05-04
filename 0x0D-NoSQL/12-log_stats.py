786bf542d9d898c5e43e8ad6e993737fc231c720
veeteeran <vietnamtran@gmail.com>
2021-05-04 09:04:34 -0700

    Wrap in main guadrd

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -3,24 +3,25 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
-db = client.logs
-collection = db.nginx
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
 
-total = collection.count_documents({})
-get = collection.count_documents({'method': 'GET'})
-post = collection.count_documents({'method': 'POST'})
-put = collection.count_documents({'method': 'PUT'})
-patch = collection.count_documents({'method': 'PATCH'})
-delete = collection.count_documents({'method': 'DELETE'})
-status = collection.count_documents({'method': 'GET', 'path': '/status'})
-nl = '\n'
+    total = collection.count_documents({})
+    get = collection.count_documents({'method': 'GET'})
+    post = collection.count_documents({'method': 'POST'})
+    put = collection.count_documents({'method': 'PUT'})
+    patch = collection.count_documents({'method': 'PATCH'})
+    delete = collection.count_documents({'method': 'DELETE'})
+    status = collection.count_documents({'method': 'GET', 'path': '/status'})
+    nl = '\n'
 
-print(f"{total} logs{nl}\
-Methods:{nl}\
-\tmethod GET: {get}{nl}\
-\tmethod POST: {post}{nl}\
-\tmethod PUT: {put}{nl}\
-\tmethod PATCH: {patch}{nl}\
-\tmethod DELETE: {delete}{nl}\
+    print(f"{total} logs{nl}\
+    Methods:{nl}\
+    \tmethod GET: {get}{nl}\
+    \tmethod POST: {post}{nl}\
+    \tmethod PUT: {put}{nl}\
+    \tmethod PATCH: {patch}{nl}\
+    \tmethod DELETE: {delete}{nl}\
 {status} status check")
