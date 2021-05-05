b90aa0bc1ec704f07157addf1d311cc23b168ba2
Viet <vietnamtran@gmail.com>
2021-05-05 16:33:32 -0500

    Add code to test

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const createIteratorObject = (report) => {
+    let arr = [];
+    for (let key in report) {
+        arr.push(...report[key]);
+    }
+
+    return arr;
+}
+
+export default createIteratorObject
