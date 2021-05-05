479dc2c54f476756bc8c3ecd6a41c898e24ba6b6
Viet <vietnamtran@gmail.com>
2021-05-05 11:32:13 -0500

    Add code to test output

--- 0x0E-ES6_basic/9-getFullBudget.js
+++ 0x0E-ES6_basic/9-getFullBudget.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+import getBudgetObject from './7-getBudgetObject.js';
+
+const getFullBudgetObject = (income, gdp, capita) => {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: function (income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros: function (income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
+
+export default getFullBudgetObject;
