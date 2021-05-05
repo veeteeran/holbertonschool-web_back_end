51dd3d477b2d16bf8ec7c975f9e978be5510193f
Viet <vietnamtran@gmail.com>
2021-05-05 09:39:28 -0500

    Rewrite using arrow syntax

--- 0x0E-ES6_basic/2-arrow.js
+++ 0x0E-ES6_basic/2-arrow.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const getNeighborhoodsList = () => {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
+    const self = this;
+    this.addNeighborhood = function add(newNeighborhood) {
+      self.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return self.sanFranciscoNeighborhoods;
+    };
+}
+  
+export default getNeighborhoodsList;
