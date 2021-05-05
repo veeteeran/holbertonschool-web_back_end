f89f9678159bcc6acbd241e662118111aa976c81
Viet <vietnamtran@gmail.com>
2021-05-05 14:07:12 -0500

    Add code to test

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const createReportObject = employeesList => {
+    const returnObj = {
+        allEmployees: [...employeesList],
+        getNumberOfDepartments: 0,
+    }
+
+    return (returnObj);
+}
