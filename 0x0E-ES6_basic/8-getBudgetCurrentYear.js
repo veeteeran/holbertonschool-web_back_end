eb0cc345546ecbf852640a0c6ec251070b7e00cf
Viet <vietnamtran@gmail.com>
2021-05-05 11:17:07 -0500

    Add computed prop names

--- 0x0E-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x0E-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const getCurrentYear = () => {
+    const date = new Date();
+    return date.getFullYear();
+  }
+  
+  const getBudgetForCurrentYear = (income, gdp, capita) => {
+      const budget = {
+          [`income-${getCurrentYear()}`]: income,
+          [`gdp-${getCurrentYear()}`]: gdp,
+          [`capita-${getCurrentYear()}`]: capita,
+    };
+  
+    return budget;
+  }
+
+export default getBudgetForCurrentYear;
