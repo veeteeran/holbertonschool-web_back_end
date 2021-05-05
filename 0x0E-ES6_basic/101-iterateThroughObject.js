0383ed96b2c6e0032c70188eb35391af316f170f
Viet <vietnamtran@gmail.com>
2021-05-05 17:53:47 -0500

    Add code to test

--- 0x0E-ES6_basic/101-iterateThroughObject.js
+++ 0x0E-ES6_basic/101-iterateThroughObject.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const iterateThroughObject = (reportWithIterator) => {
+    for (let i = 0; i < reportWithIterator.length; i++) {
+        i < reportWithIterator.length - 1 ? console.log(`${reportWithIterator[i]} | `) : console.log(reportWithIterator[i]);
+    }
+}
+
+export default iterateThroughObject;
