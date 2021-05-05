4d16c3f248ed6c88c1caaff9e5aff94b35b96231
Viet <vietnamtran@gmail.com>
2021-05-05 11:39:22 -0500

    Use for of lop

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const appendToEachArrayValue = (array, appendString) => {
+    for (value of array) {
+      value = appendString + value;
+    }
+  
+    return array;
+}
+  
+export default appendToEachArrayValue;
