1c9840c4af9f4e4555555d6d22d9a3a1800aa0d4
veeteeran <vietnamtran@gmail.com>
2021-05-06 16:27:19 +0000

    Separate obeject attributes on separate lines

--- 0x0E-ES6_basic/7-getBudgetObject.js
+++ 0x0E-ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,11 @@
-export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+const getBudgetObject = (income, gdp, capita) => {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
   return budget;
-}
+};
+
+export default getBudgetObject;
