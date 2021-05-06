3828391dbdd60d28693917068161c3e08460e4a1
veeteeran <vietnamtran@gmail.com>
2021-05-06 15:58:07 +0000

    Eslint fixes

--- 0x0E-ES6_basic/3-default-parameter.js
+++ 0x0E-ES6_basic/3-default-parameter.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-const getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
+const getSumOfHoods = (
+  initialNumber,
+  expansion1989 = 89,
+  expansion2019 = 19,
+) => initialNumber + expansion1989 + expansion2019;
 
 export default getSumOfHoods;
