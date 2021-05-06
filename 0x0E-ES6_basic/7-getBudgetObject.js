36094e4e659cfd054eaf9460b529826638291b6b
veeteeran <vietnamtran@gmail.com>
2021-05-06 16:22:04 +0000

    Use non-arrow syntax

--- 0x0E-ES6_basic/7-getBudgetObject.js
+++ 0x0E-ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,4 @@
-#!/usr/bin/node
-const getBudgetObject = (income, gdp, capita) => {
+export default function getBudgetObject(income, gdp, capita) {
   const budget = { income, gdp, capita };
   return budget;
 };
-
-export default getBudgetObject;
