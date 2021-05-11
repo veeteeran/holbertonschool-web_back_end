116c3f21e08d136cf9f61499950ceaa24a9eb1b6
Viet <vietnamtran@gmail.com>
2021-05-10 20:14:10 -0500

    Add handleResponseFromAPI

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({
+        status: 200,
+        body: 'success',
+      });
+    } else {
+      reject(new Error());
+    }
+  }).then(() => console.log("Got a response from the API"))
+}
