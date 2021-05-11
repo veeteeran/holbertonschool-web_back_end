f739781f5a1ea59251fff310e6a419a0aad54eb9
Viet <vietnamtran@gmail.com>
2021-05-10 21:25:03 -0500

    Add divideFunction

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function divideFunction(numerator, denominator) {
+  try {
+    const result = numerator / denominator
+    throw new Error('cannot divide by 0')
+  }
+  catch (error) {
+    console.log(error)
+  }
+
+  return result
+}
