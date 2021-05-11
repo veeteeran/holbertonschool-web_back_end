f08d788c0ea8edbea2b8fcbfa8c71d0f1e854ac3
Viet <vietnamtran@gmail.com>
2021-05-10 20:26:03 -0500

    Add handleProfileSignup

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils.js'
+
+export default function handleProfileSignup() {
+  return Promise.all([
+    uploadPhoto,
+    createUser
+  ])
+  .then(messages => console.log(messages))
+  .catch(console.log("Signup system offline"))
+}
