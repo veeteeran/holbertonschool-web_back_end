5f7f900e7baa41a3fd1dfe9177b6cc8ce1854afd
veeteeran <vietnamtran@gmail.com>
2021-05-11 02:51:05 +0000

    Eslint fixes

--- 0x0F-ES6_promise/9-try.js
+++ 0x0F-ES6_promise/9-try.js
@@ -1,8 +1,10 @@
 export default function guardrail(mathFunction) {
-  let queue = [];
+  const queue = [];
   try {
-    queue.push(mathFunction, 'Guardrail was processed')
+    queue.push(mathFunction(), 'Guardrail was processed');
   } catch (error) {
-    queue.push(error, 'Guardrail was processed')
+    queue.push(`Error: ${error.message}`, 'Guardrail was processed');
   }
+
+  return queue;
 }
