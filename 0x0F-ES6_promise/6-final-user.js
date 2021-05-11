ed6ec74c476d89ee01697bb5733e8aeacf946448
Viet <vietnamtran@gmail.com>
2021-05-10 21:01:13 -0500

    add handleProfileSignup

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
+
+export default function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName) {
+  const promises = [signUpUser(firstName, lastName),
+    uploadPhoto(fileName)]
+  
+  return Promise.allSettled(promises)
+  .then(values => console.log(values))
+  }
