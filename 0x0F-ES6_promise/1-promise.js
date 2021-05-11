746f7a744ab66ae517da1cd5e138c3eafb9036c0
Viet <vietnamtran@gmail.com>
2021-05-10 19:57:39 -0500

    Add getFullResponseFromAPI

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        "status": 200,
+        "body": "success"
+      })
+    } else {
+      reject("The fake API is not working currently")
+    }
+  })
+}
