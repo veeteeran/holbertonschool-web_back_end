02300eeafd5c3e7dc9248d220b6c3f191a8375b2
Viet <vietnamtran@gmail.com>
2021-05-10 21:44:54 -0500

    Add guardrail

--- 0x0F-ES6_promise/9-try.js
+++ 0x0F-ES6_promise/9-try.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  let queue = [];
+  try {
+    queue.push(mathFunction, 'Guardrail was processed')
+  } catch (error) {
+    queue.push(error, 'Guardrail was processed')
+  }
+}
