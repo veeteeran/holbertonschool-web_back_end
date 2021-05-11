8d76d22517f3a587b762b0945f7cc3c71067a48c
veeteeran <vietnamtran@gmail.com>
2021-05-11 01:37:30 +0000

    Fix import statement

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,14 +1,14 @@
-import { uploadPhoto, createUser } from './utils.js'
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([
     uploadPhoto(),
-    createUser()
+    createUser(),
   ])
-    .then(messages => console.log(
+    .then((messages) => console.log(
       messages[0].body,
       messages[1].firstName,
-      messages[1].lastName
+      messages[1].lastName,
     ))
-  .catch(console.log("Signup system offline"))
+    .catch(console.log('Signup system offline'));
 }
