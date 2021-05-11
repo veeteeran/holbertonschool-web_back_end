61a8e7254f1e5e63d3ee0e48bd52e1d65dfc7e00
veeteeran <vietnamtran@gmail.com>
2021-05-11 00:51:04 +0000

    Eslint fixes

--- 0x0F-ES6_promise/0-promise.js
+++ 0x0F-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve('true')
-  })
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve('true');
+  });
 }
