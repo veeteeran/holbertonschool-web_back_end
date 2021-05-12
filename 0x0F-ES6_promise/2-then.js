f330d121bbc3ce12950f9f0cc08cc76bf45a94ac
veeteeran <vietnamtran@gmail.com>
2021-05-12 00:01:32 +0000

    Add finally eslint fixes

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then({
-        status: 200,
-        body: 'success',
-    })
+    status: 200,
+    body: 'success',
+  })
     .catch(new Error())
-    .then(() => console.log('Got a response from the API'));
+    .finally(() => console.log('Got a response from the API'));
 }
