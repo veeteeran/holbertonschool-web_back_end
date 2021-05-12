c5b5f5d6b1a8528457194c2070d2d71c0515864d
Viet <vietnamtran@gmail.com>
2021-05-12 16:47:22 -0500

    Add SkyHighFloors

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+import Building from './5-building';
+
+class SkyHighBuilding extends Building {
+  constructor(floors) {
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    this._floors = newFloors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
+
+export default SkyHighBuilding;
