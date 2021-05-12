321f04dd24d617e71c8a014ccc2c0432b7c4091f
Viet <vietnamtran@gmail.com>
2021-05-12 16:32:11 -0500

    Add class Building

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft
+  }
+
+
+}
+
+export default Building;
