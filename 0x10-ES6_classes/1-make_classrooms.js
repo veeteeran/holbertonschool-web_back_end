3944767e65185460c5fa40ee32e4864fb9b6afe5
Viet <vietnamtran@gmail.com>
2021-05-12 14:32:28 -0500

    Add initializeRooms

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+import ClassRoom from './0-classroom';
+
+const initializeRooms = () => {
+  const array = [ClassRoom(19), ClassRoom(20), ClassRoom(34)];
+  return array;
+}
+
+export default initializeRooms;
