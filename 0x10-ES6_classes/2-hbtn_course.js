d5088505aa0ed87a9d8dbc3e2e5dcdff9bffe9b9
Viet <vietnamtran@gmail.com>
2021-05-12 15:14:27 -0500

    Uncomment last condition

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -4,12 +4,11 @@ class HolbertonCourse {
       throw TypeError('Name must be a string')
     } else if (typeof(length) !== 'number') {
       throw TypeError('Length must be a number')
+    } else if (typeof (students !== 'object') || (
+      students.every(e => typeof(e) !== 'string')
+    )) {
+      throw TypeError('Students must be an array of strings')
     }
-    // else if (typeof (students !== 'object') || (
-    //   students.every(e => typeof(e) !== 'string')
-    // )) {
-    //   throw TypeError('Students must be an array of strings')
-    // }
 
     this._name = name;
     this._length = length;
