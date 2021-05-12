42af847f3785bd61cfebb7b1595ed96a788ec278
veeteeran <vietnamtran@gmail.com>
2021-05-12 00:45:21 +0000

    Fix console log in then

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,12 +3,10 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([
     uploadPhoto(),
-    createUser(),
-  ])
-    .then((messages) => console.log(
-      messages[0].body,
-      messages[1].firstName,
-      messages[1].lastName,
-    ))
-    .catch(() => console.log('Signup system offline'));
+    createUser()])
+    .then((messages) => {
+      console.log(
+        `${messages[0].body} ${messages[1].firstName} ${messages[1].lastName}`,
+      );
+    }).catch(() => console.log('Signup system offline'));
 }
