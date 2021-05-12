5765788226cda554d66b6dabf8e3dd8a0ece7b52
Viet <vietnamtran@gmail.com>
2021-05-12 15:07:38 -0500

    Add HOlbertonCourse

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,1 +1,43 @@
-#!/usr/bin/node
+class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof(name) !== 'string') {
+      throw TypeError('Name must be a string')
+    } else if (typeof(length) !== 'number') {
+      throw TypeError('Length must be a number')
+    } else if (typeof (students !== 'object') || (
+      students.every(e => typeof(e) !== 'string')
+    )) {
+      throw TypeError('Students must be an array of strings')
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set changeName(newName) {
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set changeLength(newLength) {
+    this._length = newLength
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set changeStudents(newStudent) {
+    this._students.push(newStudent);
+  }
+}
+  
+export default HolbertonCourse;
