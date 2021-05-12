dfd99989e1d797b6cd1377dbe78ec29b83e7d208
Viet <vietnamtran@gmail.com>
2021-05-12 15:50:56 -0500

    Add class Currency

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,1 +1,28 @@
-#!/usr/bin/node
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`
+  }
+}
+
+export default Currency;
