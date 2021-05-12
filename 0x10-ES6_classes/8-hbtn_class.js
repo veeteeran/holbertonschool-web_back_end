942e9606dda24a96a386d7028d1cfd789fdd150f
Viet <vietnamtran@gmail.com>
2021-05-12 17:53:15 -0500

    Add class HolbertonClass

--- 0x10-ES6_classes/8-hbtn_class.js
+++ 0x10-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
+
+export default HolbertonClass;
