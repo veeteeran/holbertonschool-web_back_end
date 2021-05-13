432bce7febeaea309e2d7bf2583dad10b996fa5b
Viet <vietnamtran@gmail.com>
2021-05-12 21:10:03 -0500

    Add EVCar

--- 0x10-ES6_classes/100-evcar.js
+++ 0x10-ES6_classes/100-evcar.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+import Car from './10-car'
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+    this._range = range;
+  }
+
+  static get [Symbol.species]() { return Car; }
+}
+
+export default EVCar;
