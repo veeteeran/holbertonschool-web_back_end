ef5321e2d65f8c252ab4e5a6de8d51320b8f9da0
veeteeran <vietnamtran@gmail.com>
2021-05-13 00:00:34 +0000

    Use displayFullCurrency method

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -21,7 +21,7 @@ class Pricing {
   }
 
   displayFullPrice() {
-    return `${this.amount} ${this.currency.name} ${this.currency.code}`;
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
