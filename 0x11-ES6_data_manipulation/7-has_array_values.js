66a91bc5ec2aad8e49d5351c9f7b1da6a219fe76
Viet <vietnamtran@gmail.com>
2021-05-17 13:36:13 -0500

    Add hasValuesFromArray

--- 0x11-ES6_data_manipulation/7-has_array_values.js
+++ 0x11-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const hasValuesFromArray = (set, array) => {
+  let result = true;
+  array.forEach(element => {
+    if (!set.has(element)) {
+      result = false;
+      return;
+    }
+  });
+
+  return result;
+}
+
+export default hasValuesFromArray;
