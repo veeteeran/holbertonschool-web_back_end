42d80191baf20241a8ac164ac2ed5b2224e89de1
Viet <vietnamtran@gmail.com>
2021-05-17 16:27:08 -0500

    Add weakMap, queryAPI

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const weakMap = new WeakMap();
+const queryAPI = (endpoint) => {
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  }
+}
+
+export { weakMap, queryAPI };
