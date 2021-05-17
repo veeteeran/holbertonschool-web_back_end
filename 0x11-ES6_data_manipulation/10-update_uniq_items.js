1f09039163065bdc6475d429f4259852f70e1eb5
Viet <vietnamtran@gmail.com>
2021-05-17 16:03:07 -0500

    Add updateUniqueItems

--- 0x11-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+const updateUniqueItems = (map) => {
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100)
+    }
+  });
+
+  return map;
+}
+
+export default updateUniqueItems;
