1c51e270eef14223a1a7904166ce1ad6fb811905
veeteeran <vietnamtran@gmail.com>
2021-05-17 16:40:34 +0000

    Eslint fix

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,13 @@
-const updateStudentGradeByCity = (students, city, grades) => {
-  let result;
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      let studentGrade;
-      grades.forEach(grade => {
-        if (grade.studentId === student.id) {
-          studentGrade = grade.grade;
-        }
-      })
-      return studentGrade ? { ...student, grade: studentGrade } : { ...student, grade: 'N/A' }
-    }
-    )
-}
+const updateStudentGradeByCity = (students, city, grades) => students
+  .filter((student) => student.location === city)
+  .map((student) => {
+    let studentGrade;
+    grades.forEach((grade) => {
+      if (grade.studentId === student.id) {
+        studentGrade = grade.grade;
+      }
+    });
+    return studentGrade ? { ...student, grade: studentGrade } : { ...student, grade: 'N/A' };
+  });
 
 export default updateStudentGradeByCity;
