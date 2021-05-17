1b92476be6b54e72ba3e68d1e9936ce4c374665a
Viet <vietnamtran@gmail.com>
2021-05-17 14:16:25 -0500

    Add cleanSet

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+const cleanSet = (set, startString) => {
+  let result = '';
+
+  if (startString === '') return result;
+
+  set.forEach(s => {
+    if (s.startsWith(startString)) {
+      if (result === '') {
+        result += s.substring(startString.length);
+      } else {
+        result += `-${s.substring(startString.length)}`;
+      }
+    }
+  })
+
+  return result;
+}
+
+export default cleanSet;
