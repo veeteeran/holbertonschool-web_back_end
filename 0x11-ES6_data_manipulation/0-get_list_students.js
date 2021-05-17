fde3b6cf209b29c1903c4fa0da72479790856427
Viet <vietnamtran@gmail.com>
2021-05-17 09:56:01 -0500

    Fix typo

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
 const getListStudents = () => [
-  { id: 1, firstName: 'Guillaume', location: 'San Francsico' },
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
   { id: 2, firstName: 'James', location: 'Columbia' },
   { id: 5, firstName: 'Serena', location: 'San Francisco' },
 ];
