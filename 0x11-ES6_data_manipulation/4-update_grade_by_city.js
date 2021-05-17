2a02fb584d759d5d6f77eeb68315355ed8ffc1c3
Viet <vietnamtran@gmail.com>
2021-05-17 11:39:07 -0500

    Add updateStudentGradeByCity

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+const updateStudentGradeByCity = (students, city, grades) => {
+  let result;
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      let studentGrade;
+      grades.forEach(grade => {
+        if (grade.studentId === student.id) {
+          studentGrade = grade.grade;
+        }
+      })
+      return studentGrade ? { ...student, grade: studentGrade } : { ...student, grade: 'N/A' }
+    }
+    )
+}
+
+export default updateStudentGradeByCity;
