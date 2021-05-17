ded73c17d2d59d02e5f5fa99dead4886e419d277
Viet <vietnamtran@gmail.com>
2021-05-17 10:13:51 -0500

    Add getListStudentIds

--- 0x11-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+import getListStudentIds from "./1-get_list_student_ids";
+
+const getStudentIdsSum = (students) => {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const ids = getListStudentIds(students);
+  return ids.reduce(reducer);
+}
+
+export default getStudentIdsSum;
