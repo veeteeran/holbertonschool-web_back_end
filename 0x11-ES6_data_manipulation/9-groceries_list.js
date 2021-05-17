fc67011ec3731cd06d1868dc6fc4f5e80f3668c0
Viet <vietnamtran@gmail.com>
2021-05-17 15:54:27 -0500

    Add groceriesList

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+const groceriesList = () => {
+  return new Map()
+    .set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 1)
+    .set('Rice', 1)
+    .set('Banana', 5)
+}
+
+export default groceriesList;
