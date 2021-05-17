ed41b5ef5fa73310f82632d312f23ceef43e4771
veeteeran <vietnamtran@gmail.com>
2021-05-17 17:12:43 +0000

    Refactor code, eslint fixes

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,16 @@
 const createInt8TypedArray = (length, position, value) => {
-  let buffer = new ArrayBuffer(length);
-  let int8View = new Int8Array(buffer, position, value);
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
 
-  if (position > length - 1) {
-    throw 'Position outside range'
-  } else {
+  if (position > length - 1) throw Error('Position outside range');
+
+  try {
     int8View[position] = value;
+  } catch (e) {
+    console.logerror(e);
   }
 
   return buffer;
-}
+};
+
+export default createInt8TypedArray;
