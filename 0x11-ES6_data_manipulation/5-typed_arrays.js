5d3aea0ef3e321104ecfaa9fe473181e5e4736d4
Viet <vietnamtran@gmail.com>
2021-05-17 12:02:53 -0500

    Add createInt8TypedArray

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+const createInt8TypedArray = (length, position, value) => {
+  let buffer = new ArrayBuffer(length);
+  let int8View = new Int8Array(buffer, position, value);
+
+  if (position > length - 1) throw 'Position outside range'
+
+  int8View[position] = value;
+
+  return int8View;
+}
