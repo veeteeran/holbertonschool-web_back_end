6526ab528c88a8cd591e1f9db82f3efbfded4c31
veeteeran <vietnamtran@gmail.com>
2021-05-19 01:10:07 +0000

    Eslint fixes

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,9 @@ const cleanSet = (set, startString) => {
   if (startString === '') return '';
 
   return [...set]
-    .filter(s => s.startsWith(startString))
-    .map(value => value.substring(startString.length))
-    .join('-')
-}
+    .filter((s) => s.startsWith(startString))
+    .map((value) => value.substring(startString.length))
+    .join('-');
+};
 
 export default cleanSet;
