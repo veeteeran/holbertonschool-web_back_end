4904b0b4b160ee20e43bf281f35cad8541f5d7f8
Viet <vietnamtran@gmail.com>
2021-05-18 20:27:27 -0500

    Add check or type

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 const cleanSet = (set, startString) => {
   if (startString === '') return '';
+  if (set instanceof Set === false) return '';
+  if (typeof startString !== 'string') return '';
 
   return [...set]
     .filter(s => s.startsWith(startString))
