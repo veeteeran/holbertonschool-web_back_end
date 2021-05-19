034e6afc7345e70cc8d99f8c25cb769f4b8eb8c3
veeteeran <vietnamtran@gmail.com>
2021-05-19 00:27:52 +0000

    Eslint fix

--- 0x11-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,10 +1,10 @@
-import getListStudentIds from "./1-get_list_student_ids";
+import getListStudentIds from './1-get_list_student_ids';
 
 const getStudentIdsSum = (students) => {
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
   const ids = getListStudentIds(students);
 
   return ids.reduce(reducer);
-}
+};
 
 export default getStudentIdsSum;
