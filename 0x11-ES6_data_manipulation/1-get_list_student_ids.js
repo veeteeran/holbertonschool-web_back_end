51780cf55f3eb3772a3aee71478f5b46f8aec6a5
veeteeran <vietnamtran@gmail.com>
2021-05-19 00:24:37 +0000

    Eslint fix

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 const getListStudentIds = (objects) => {
   if (!Array.isArray(objects)) return [];
 
-  return objects.map(object => object.id)
-}
+  return objects.map((object) => object.id);
+};
 
 export default getListStudentIds;
