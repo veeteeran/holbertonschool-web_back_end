407b02d9504b0d6b68eb9504abb7a3b89e3d6367
veeteeran <vietnamtran@gmail.com>
2021-05-19 00:51:43 +0000

    Use get instesd of has

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ const weakMap = new WeakMap();
 const queryAPI = (endpoint) => {
   if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
 
-  if (!weakMap.has(endpoint)) {
+  if (!weakMap.get(endpoint)) {
     weakMap.set(endpoint, 1);
   } else {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
