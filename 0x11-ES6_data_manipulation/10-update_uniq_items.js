50ce6fc1282e4b752f292db1477e5a61b1a03fbe
veeteeran <vietnamtran@gmail.com>
2021-05-19 00:47:25 +0000

    Eslint fixes

--- 0x11-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 const updateUniqueItems = (map) => {
-    if (!(map instanceof Map)) throw Error('Cannot process');
-    map.forEach((value, key) => {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  map.forEach((value, key) => {
     if (value === 1) {
       map.set(key, 100);
     }
