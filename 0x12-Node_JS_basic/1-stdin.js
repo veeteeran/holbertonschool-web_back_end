9b2098b90c0b0f507983c37e4a398a565e8b2acd
Viet <vietnamtran@gmail.com>
2021-05-19 20:30:50 -0500

    Add User prompt

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+process.stdout.write(
+  'Welcome to Holberton School, what is your name?' + '\n'
+)
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  let chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(
+      `Your name is: ${chunk}`
+    )
+  }
+})
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing' + '\n')
+})
