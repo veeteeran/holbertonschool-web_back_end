5f5d5f83c93f3841ec37d7304941747e61ad0aa3
Viet <vietnamtran@gmail.com>
2021-05-19 21:34:47 -0500

    Add countStudents

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,1 +1,33 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    const lines = data.split(/\r?\n/);
+    lines.shift();
+    lines.pop();
+
+    console.log(`Number of students: ${lines.length}`)
+
+    const cs = lines
+      .filter(line => line.endsWith('CS'))
+      .map(line => {
+        const student = line.split(',');
+        return student[0];
+      })
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`)
+
+    const swe = lines
+      .filter(line => line.endsWith('SWE'))
+      .map(line => {
+        const student = line.split(',');
+        return student[0];
+      })
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`)
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
