d5b8e03fc1af6949cb5a41d11998254ee4d6b968
Viet <vietnamtran@gmail.com>
2021-05-21 17:54:14 -0500

    Add more complex express http server

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -1,1 +1,31 @@
-#!/usr/bin/node
+const countStudents = require('./3-read_file_async');
+
+const express = require('express');
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+})
+
+app.get('/students', async (req, res) => {
+  res.status(200);
+  res.set('Content-Type', 'text/plain');
+  await countStudents(process.argv[2])
+    .then(value => {
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${value.lines.length}\n`);
+      res.write(`Number of students in CS: ${value.cs.length}. List: ${value.cs.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${value.swe.length}. List: ${value.swe.join(', ')}`);
+    })
+    .catch(console.error)
+  res.end();
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+module.export = app;
