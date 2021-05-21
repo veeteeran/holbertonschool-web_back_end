3f87391100feeba4abc7223b9efe162aa6976165
Viet <vietnamtran@gmail.com>
2021-05-21 17:09:30 -0500

    Refactor without .then, return data

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -2,35 +2,31 @@ const fsp = require('fs/promises');
 
 const countStudents = async (path) => {
   try {
-    const promise = fsp.readFile(path, 'utf8');
-    await promise;
+    const promise = await fsp.readFile(path, 'utf8');
 
-    promise
-      .then(value => {
-        let lines = value.split(/\r?\n/);
-        lines.shift();
-        lines = lines.filter(line => line !== '');
+    let lines = promise.split(/\r?\n/);
+    lines.shift();
+    lines = lines.filter(line => line !== '');
 
-        console.log(`Number of students: ${lines.length}`)
+    console.log(`Number of students: ${lines.length}`)
 
-        const cs = lines
-          .filter(line => line.endsWith('CS'))
-          .map(line => {
-            const student = line.split(',');
-            return student[0];
-          })
-        console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`)
-
-        const swe = lines
-          .filter(line => line.endsWith('SWE'))
-          .map(line => {
-            const student = line.split(',');
-            return student[0];
-          })
-        console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`)
+    const cs = lines
+      .filter(line => line.endsWith('CS'))
+      .map(line => {
+        const student = line.split(',');
+        return student[0];
       })
-      .catch((e) => console.log(e));
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`)
 
+    const swe = lines
+      .filter(line => line.endsWith('SWE'))
+      .map(line => {
+        const student = line.split(',');
+        return student[0];
+      })
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`)
+
+    return { lines, cs, swe }
   } catch (err) {
     throw Error('Cannot load the database');
   }
