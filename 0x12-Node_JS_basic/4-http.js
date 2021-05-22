e418c33033621777325a879e388e5cf8c9b032ef
veeteeran <vietnamtran@gmail.com>
2021-05-22 15:45:08 +0000

    Remove console log in listen

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
@@ -9,8 +8,6 @@ const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port);
 
 module.exports = app;
