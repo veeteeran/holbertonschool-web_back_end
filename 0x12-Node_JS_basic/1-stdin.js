a640b92fc6d49468d36e469290ddcec2688ef50e
veeteeran <vietnamtran@gmail.com>
2021-05-22 15:30:41 +0000

    Eslint fixes

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,19 +1,19 @@
 process.stdout.write(
-  'Welcome to Holberton School, what is your name?' + '\n'
-)
+  'Welcome to Holberton School, what is your name?\n',
+);
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-  let chunk = process.stdin.read();
+  const chunk = process.stdin.read();
 
   if (chunk) {
     process.stdout.write(
-      `Your name is: ${chunk}`
-    )
+      `Your name is: ${chunk}`,
+    );
   }
-})
+});
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing' + '\n')
-})
+  process.stdout.write('This important software is now closing\n');
+});
--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -4,30 +4,30 @@ const countStudents = (path) => {
   try {
     const data = fs.readFileSync(path, 'utf8');
 
-    const lines = data.split(/\r?\n/);
+    let lines = data.split(/\r?\n/);
     lines.shift();
-    lines = lines.filter(line => line !== '');
+    lines = lines.filter((line) => line !== '');
 
-    console.log(`Number of students: ${lines.length}`)
+    console.log(`Number of students: ${lines.length}`);
 
     const cs = lines
-      .filter(line => line.endsWith('CS'))
-      .map(line => {
+      .filter((line) => line.endsWith('CS'))
+      .map((line) => {
         const student = line.split(',');
         return student[0];
-      })
-    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`)
+      });
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
 
     const swe = lines
-      .filter(line => line.endsWith('SWE'))
-      .map(line => {
+      .filter((line) => line.endsWith('SWE'))
+      .map((line) => {
         const student = line.split(',');
         return student[0];
-      })
-    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`)
+      });
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
   } catch (err) {
     throw Error('Cannot load the database');
   }
-}
+};
 
 module.exports = countStudents;
