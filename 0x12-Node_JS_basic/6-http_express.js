c6a956adb36d93157cd1325c5df1c75eb74e7d48
Viet <vietnamtran@gmail.com>
2021-05-22 10:48:09 -0500

    Add status code and set header

--- 0x12-Node_JS_basic/6-http_express.js
+++ 0x12-Node_JS_basic/6-http_express.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
+  res.status(200);
+  res.set('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
