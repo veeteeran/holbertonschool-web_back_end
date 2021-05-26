575232661066558555ffc018e365b01b163faadd
Viet <vietnamtran@gmail.com>
2021-05-25 22:37:57 -0500

    Add code and tests for task 4

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+
+import sinon from "sinon";
+import Utils from './utils';
+import sendPaymentRequestToApi from './3-payment';
+const should = require('chai').should();
+
+describe('#sendPaymentRequestToApi()', function () {
+  it('should validate the usage of the Utils function', function () {
+    const spy = sinon.spy(console, 'log');
+    const stub = sinon.stub(Utils, 'calculateNumber');
+    stub.returns(10);
+    sendPaymentRequestToApi(100, 20);
+
+    spy.calledWith('The total is: 10').should.equal(true);
+    stub.calledWith('SUM', 100, 20).should.equal(true);
+    stub.alwaysReturned(10).should.equal(true);
+    spy.restore();
+    stub.restore();
+  })
+});
