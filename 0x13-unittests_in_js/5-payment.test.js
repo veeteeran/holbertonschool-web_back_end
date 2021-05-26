fb72cae1addafe553038784ad02922464cc3ec42
Viet <vietnamtran@gmail.com>
2021-05-26 17:32:13 -0500

    Add task 5

--- 0x13-unittests_in_js/5-payment.test.js
+++ 0x13-unittests_in_js/5-payment.test.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require('./5-payment');
+const should = require('chai').should();
+
+describe('#sendPaymentRequestToApi()', function () {
+  let spy;
+  beforeEach(function () {
+    spy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function () {
+    spy.calledOnce.should.equal(true);
+    spy.restore();
+  });
+
+  it('should output "The total is: 120"', function () {
+    sendPaymentRequestToApi(100, 20);
+    spy.calledWith('The total is: 120').should.equal(true);
+  });
+
+  it('should output "The total is: 20"', function () {
+    sendPaymentRequestToApi(10, 10);
+    spy.calledWith('The total is: 120').should.equal(true);
+  });
+
+});
