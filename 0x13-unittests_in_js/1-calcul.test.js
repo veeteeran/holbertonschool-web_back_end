c241e7ca5d7aaea7c18e2febe791dee38b680219
Viet <vietnamtran@gmail.com>
2021-05-26 15:50:56 -0500

    Use QA tests

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -1,68 +1,93 @@
 const assert = require('assert');
 import calculateNumber from './1-calcul';
 
-describe('#calculateNumber()', function () {
-  it('should return 5 when calculateNumber adds 1 and 3.7', function () {
-    assert.equal(calculateNumber('SUM', 1, 3.7), 5);
-  });
-  it('should return 5 when calculateNumber adds 1 and 3.7', function () {
-    assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
-  });
-  it('should return 6 when calculateNumber adds 1 and 3.7', function () {
-    assert.equal(calculateNumber('SUM', 1.5, 3.7), 6);
-  });
-  it('should return 3 when calculateNumber adds -1 and 3.7', function () {
-    assert.equal(calculateNumber('SUM', -1, 3.7), 3);
-  });
-  it('should return 0 when calculateNumber adds 0.1 and 0.3', function () {
-    assert.equal(calculateNumber('SUM', 0.1, 0.3), 0);
-  });
-  it('should return -5 when calculateNumber adds -1 and -3.7', function () {
-    assert.equal(calculateNumber('SUM', -1, -3.7), -5);
-  });
-  it('should return -1 when calculateNumber adds -0.1 and -0.7', function () {
-    assert.equal(calculateNumber('SUM', -0.1, -0.7), -1);
-  });
-  it('should return -3 when calculateNumber subtracts 1 and 3.7', function () {
-    assert.equal(calculateNumber('SUBTRACT', 1, 3.7), -3);
-  });
-  it('should return 3 when calculateNumber subtracts 3.7 and 1.2', function () {
-    assert.equal(calculateNumber('SUBTRACT', 3.7, 1.2), 3);
-  });
-  it('should return 2 when calculateNumber subtracts 3.7 and 1.5', function () {
-    assert.equal(calculateNumber('SUBTRACT', 3.7, 1.5), 2);
-  });
-  it('should return 5 when calculateNumber subtracts 3.7 and -1', function () {
-    assert.equal(calculateNumber('SUBTRACT', 3.7, -1), 5);
-  });
-  it('should return 0 when calculateNumber subtracts 0.1 and 0.3', function () {
-    assert.equal(calculateNumber('SUBTRACT', 0.1, 0.3), 0);
-  });
-  it('should return 3 when calculateNumber subtracts -1 and -3.7', function () {
-    assert.equal(calculateNumber('SUBTRACT', -1, -3.7), 3);
-  });
-  it('should return 1 when calculateNumber subtracts -0.7 and -0.1', function () {
-    assert.equal(calculateNumber('SUBTRACT', -0.7, -0.1), -1);
-  });
-  it('should return 0.25 when calculateNumber divides 1 and 3.7', function () {
-    assert.equal(calculateNumber('DIVIDE', 1, 3.7), 0.25);
-  });
-  it('should return 4 when calculateNumber divides 3.7 and 1.2', function () {
-    assert.equal(calculateNumber('DIVIDE', 3.7, 1.2), 4);
-  });
-  it('should return 2 when calculateNumber divides 3.7 and 1.5', function () {
-    assert.equal(calculateNumber('DIVIDE', 3.7, 1.5), 2);
-  });
-  it('should return -4 when calculateNumber divides 3.7 and -1', function () {
-    assert.equal(calculateNumber('DIVIDE', 3.7, -1), -4);
-  });
-  it('should return "Error" when calculateNumber divides 0.1 and 0.3', function () {
-    assert.equal(calculateNumber('DIVIDE', 0.1, 0.3), 'Error');
-  });
-  it('should return -1 when calculateNumber divides -1 and -3.7', function () {
-    assert.equal(calculateNumber('DIVIDE', -1, 0.7), -1);
-  });
-  it('should return "Error" when calculateNumber divides -0.7 and -0.1', function () {
-    assert.equal(calculateNumber('DIVIDE', -0.7, -0.1), 'Error');
+// describe('#calculateNumber()', function () {
+//   it('should return 5 when calculateNumber adds 1 and 3.7', function () {
+//     assert.equal(calculateNumber('SUM', 1, 3.7), 5);
+//   });
+//   it('should return 5 when calculateNumber adds 1 and 3.7', function () {
+//     assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
+//   });
+//   it('should return 6 when calculateNumber adds 1 and 3.7', function () {
+//     assert.equal(calculateNumber('SUM', 1.5, 3.7), 6);
+//   });
+//   it('should return 3 when calculateNumber adds -1 and 3.7', function () {
+//     assert.equal(calculateNumber('SUM', -1, 3.7), 3);
+//   });
+//   it('should return 0 when calculateNumber adds 0.1 and 0.3', function () {
+//     assert.equal(calculateNumber('SUM', 0.1, 0.3), 0);
+//   });
+//   it('should return -5 when calculateNumber adds -1 and -3.7', function () {
+//     assert.equal(calculateNumber('SUM', -1, -3.7), -5);
+//   });
+//   it('should return -1 when calculateNumber adds -0.1 and -0.7', function () {
+//     assert.equal(calculateNumber('SUM', -0.1, -0.7), -1);
+//   });
+//   it('should return -3 when calculateNumber subtracts 1 and 3.7', function () {
+//     assert.equal(calculateNumber('SUBTRACT', 1, 3.7), -3);
+//   });
+//   it('should return 3 when calculateNumber subtracts 3.7 and 1.2', function () {
+//     assert.equal(calculateNumber('SUBTRACT', 3.7, 1.2), 3);
+//   });
+//   it('should return 2 when calculateNumber subtracts 3.7 and 1.5', function () {
+//     assert.equal(calculateNumber('SUBTRACT', 3.7, 1.5), 2);
+//   });
+//   it('should return 5 when calculateNumber subtracts 3.7 and -1', function () {
+//     assert.equal(calculateNumber('SUBTRACT', 3.7, -1), 5);
+//   });
+//   it('should return 0 when calculateNumber subtracts 0.1 and 0.3', function () {
+//     assert.equal(calculateNumber('SUBTRACT', 0.1, 0.3), 0);
+//   });
+//   it('should return 3 when calculateNumber subtracts -1 and -3.7', function () {
+//     assert.equal(calculateNumber('SUBTRACT', -1, -3.7), 3);
+//   });
+//   it('should return 1 when calculateNumber subtracts -0.7 and -0.1', function () {
+//     assert.equal(calculateNumber('SUBTRACT', -0.7, -0.1), -1);
+//   });
+//   it('should return 0.25 when calculateNumber divides 1 and 3.7', function () {
+//     assert.equal(calculateNumber('DIVIDE', 1, 3.7), 0.25);
+//   });
+//   it('should return 4 when calculateNumber divides 3.7 and 1.2', function () {
+//     assert.equal(calculateNumber('DIVIDE', 3.7, 1.2), 4);
+//   });
+//   it('should return 2 when calculateNumber divides 3.7 and 1.5', function () {
+//     assert.equal(calculateNumber('DIVIDE', 3.7, 1.5), 2);
+//   });
+//   it('should return -4 when calculateNumber divides 3.7 and -1', function () {
+//     assert.equal(calculateNumber('DIVIDE', 3.7, -1), -4);
+//   });
+//   it('should return "Error" when calculateNumber divides 0.1 and 0.3', function () {
+//     assert.equal(calculateNumber('DIVIDE', 0.1, 0.3), 'Error');
+//   });
+//   it('should return -1 when calculateNumber divides -1 and -3.7', function () {
+//     assert.equal(calculateNumber('DIVIDE', -1, 0.7), -1);
+//   });
+//   it('should return "Error" when calculateNumber divides -0.7 and -0.1', function () {
+//     assert.equal(calculateNumber('DIVIDE', -0.7, -0.1), 'Error');
+//   });
+// });
+
+describe('calculateNumber', () => {
+  describe('when type is SUM', () => {
+    it('it round the first argument', () => {
+      assert.equal(calculateNumber('SUM', 1.0, 0), 1);
+      assert.equal(calculateNumber('SUM', 1.3, 0), 1);
+      assert.equal(calculateNumber('SUM', 1.7, 0), 2);
+    });
+
+    it('it round the second argument', () => {
+      assert.equal(calculateNumber('SUM', 0, 1.0), 1);
+      assert.equal(calculateNumber('SUM', 0, 1.3), 1);
+      assert.equal(calculateNumber('SUM', 0, 1.7), 2);
+    });
+
+    it('it should return the right number', () => {
+      assert.equal(calculateNumber('SUM', 1.3, 0), 1);
+      assert.equal(calculateNumber('SUM', 0, 1.2), 1);
+      assert.equal(calculateNumber('SUM', 1.3, 1.3), 2);
+      assert.equal(calculateNumber('SUM', 1.7, 1.2), 3);
+      assert.equal(calculateNumber('SUM', 1.3, 1.8), 3);
+      assert.equal(calculateNumber('SUM', 1.6, 1.8), 4);
+    });
   });
 });
