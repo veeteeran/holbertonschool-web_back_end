25246a1ff1b10c96fbaa78e7ea4e389be5888580
Viet <vietnamtran@gmail.com>
2021-05-25 19:15:47 -0500

    Add dependencies and tests for task 0

--- 0x13-unittests_in_js/0-calcul.js
+++ 0x13-unittests_in_js/0-calcul.js
@@ -1,1 +1,3 @@
-#!/usr/bin/node
+export default function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+}
--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const assert = require('assert');
+import calculateNumber from './0-calcul';
+
+describe('#calculateNumber()', function () {
+  it('should return 5 when calculateNumber adds 1 and 3.7', function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should return 5 when calculateNumber adds 1 and 3.7', function () {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return 6 when calculateNumber adds 1 and 3.7', function () {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it('should return 3 when calculateNumber adds -1 and 3.7', function () {
+    assert.strictEqual(calculateNumber(-1, 3.7), 3);
+  });
+  it('should return 0 when calculateNumber adds 0.1 and 0.3', function () {
+    assert.strictEqual(calculateNumber(0.1, 0.3), 0);
+  });
+  it('should return -5 when calculateNumber adds -1 and -3.7', function () {
+    assert.strictEqual(calculateNumber(-1, -3.7), -5);
+  });
+  it('should return -1 when calculateNumber adds -0.1 and -0.7', function () {
+    assert.strictEqual(calculateNumber(-0.1, -0.7), -1);
+  });
+});
