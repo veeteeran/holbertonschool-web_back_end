d5609ceafae0966b8075d0570777806f4456081b
Viet <vietnamtran@gmail.com>
2021-05-26 16:34:05 -0500

    Use requirements

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -1,7 +1,7 @@
 
-import sinon from "sinon";
-import Utils from './utils';
-import sendPaymentRequestToApi from './3-payment';
+const sinon = require("sinon");
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 const should = require('chai').should();
 
 describe('#sendPaymentRequestToApi()', function () {
