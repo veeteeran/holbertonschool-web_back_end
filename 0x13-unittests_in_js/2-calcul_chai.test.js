ca6e0681f4af66d3c3a9540df8e5e29693dd1c23
Viet <vietnamtran@gmail.com>
2021-05-26 16:15:40 -0500

    Fix typo

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const expect = require("chai").expect;
-const calculateNumber = ('./2-calcul');
+const calculateNumber = ('./2-calcul_chai');
 
 
 it('should return 5 when calculateNumber adds 1 and 3.7', function () {
