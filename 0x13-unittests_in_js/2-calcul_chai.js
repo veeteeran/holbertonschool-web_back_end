c9069e4f5a1b22978d210030e47bb9a7d6a673ee
Viet <vietnamtran@gmail.com>
2021-05-25 21:14:17 -0500

    Add check for type

--- 0x13-unittests_in_js/2-calcul_chai.js
+++ 0x13-unittests_in_js/2-calcul_chai.js
@@ -1,4 +1,6 @@
 export default function calculateNumber(type, a, b) {
+  const operations = ['SUM', 'SUBTRACT', 'DIVIDE'];
+  if (operations.indexOf(type) === -1) throw Error('Cannot perform that operation');
   if (type === 'SUM') return Math.round(a) + Math.round(b);
   if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
   if (type === 'DIVIDE') {
