fcbc0e00090944acd42e73a89f8a19b95b44c6d0
Viet <vietnamtran@gmail.com>
2021-05-26 20:50:37 -0500

    Add task 6

--- 0x13-unittests_in_js/6-payment_token.js
+++ 0x13-unittests_in_js/6-payment_token.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+function getPaymentTokenFromAPI(success) {
+  if (success) return Promise.resolve({ data: 'Successful response from the API' });
+}
+
+module.exports = getPaymentTokenFromAPI;
--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const chai = require('chai');
+const sinon = require("sinon");
+const should = chai.should();
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe.only('#getPaymentTokenFromAPI()', function () {
+  it('should return "Successful response from the API" if true passed in', function (done) {
+    getPaymentTokenFromAPI(true)
+      .then(function (value) {
+        value.data.should.equal("Successful response from the API");
+        done();
+      });
+  });
+});
