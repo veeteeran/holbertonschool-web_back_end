81310e54ec3c72d4e4d01e85ac655ccce53df043
veeteeran <vietnamtran@gmail.com>
2021-05-29 11:41:31 -0500

    Use kue to create and process jobs

--- 0x14-queuing_system_in_js/6-job_creator.js
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+import kue from 'kue'
+const queue = kue.createQueue()
+
+const jobData = {
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
+}
+
+const job = queue.create('push_notification_code', jobData)
+  .save(err => {
+    if (!err) console.log(`Notification job created: ${job.id}`)
+  })
+
+job.on("complete", result => console.log("Notification job completed"))
+
+job.on("failed", errorMessage => console.log("Notification job failed"))
