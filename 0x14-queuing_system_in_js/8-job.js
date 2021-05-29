8e571022e4c019e98cc0484e3e0d0bd5f5a8a0b2
veeteeran <vietnamtran@gmail.com>
2021-05-29 14:45:41 -0500

    Add createPushNotificationsJobs

--- 0x14-queuing_system_in_js/8-job.js
+++ 0x14-queuing_system_in_js/8-job.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.isArray(jobs)) throw Error("Jobs is not an array")
+
+  jobs.forEach(item => {
+    const job = queue.create('push_notification_code_3', item)
+      .save(err => {
+        if (!err) console.log(`Notification job created: ${job.id}`)
+      })
+    job
+      .on("complete", result => console.log(`Notification job ${job.id} completed`))
+      .on("failed", errorMessage => console.log(`Notification job ${job.id} failed: ${errorMessage}`))
+      .on("progress", (progress, data) => console.log(`Notification job ${job.id} ${progress}% complete`))
+  })
+}
+
+export default createPushNotificationsJobs
