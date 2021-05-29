d0cc619fe09aa5fac6581a079fd66fd217c4df30
veeteeran <vietnamtran@gmail.com>
2021-05-29 14:37:38 -0500

    Add new job processor

--- 0x14-queuing_system_in_js/7-job_processor.js
+++ 0x14-queuing_system_in_js/7-job_processor.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+import kue from 'kue'
+const queue = kue.createQueue()
+
+const blackList = ["4153518780", "4153518781"]
+
+const sendNotification = (phoneNumber, message, job, done) => {
+  job.progress(0, 100)
+  if (blackList.indexOf(phoneNumber) !== -1) {
+    return done(Error(`Phone number ${phoneNumber} is blacklisted`))
+  }
+
+  job.progress(50, 100)
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+
+  done()
+}
+
+queue.process("push_notification_code_2", 2, (job, done) => {
+  const { phoneNumber, message } = job.data
+  sendNotification(phoneNumber, message, job, done)
+})
