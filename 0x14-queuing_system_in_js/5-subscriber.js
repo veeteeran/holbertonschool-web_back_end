62f31629febfed77e286be751255cfac0d0bb94e
veeteeran <vietnamtran@gmail.com>
2021-05-29 10:59:19 -0500

    Add pub/sub

--- 0x14-queuing_system_in_js/5-subscriber.js
+++ 0x14-queuing_system_in_js/5-subscriber.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+import redis from "redis"
+const subscriber = redis.createClient();
+
+subscriber.on("connect", () => {
+  console.log("Redis client connected to the server")
+})
+
+subscriber.on("error", error => {
+  console.log(`Redis client not connected to the server: ${error}`)
+})
+
+subscriber.subscribe("holberton school channel")
+subscriber.on("message", (channel, message) => {
+  if (channel === "holberton school channel") console.log(message);
+  if (message === "KILL_SERVER") {
+    subscriber.unsubscribe()
+    subscriber.quit()
+  }
+})
