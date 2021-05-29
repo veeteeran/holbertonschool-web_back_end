4031d5932983fe480c061cae7f50fcc43a0082b6
veeteeran <vietnamtran@gmail.com>
2021-05-29 16:40:59 -0500

    Move .save

--- 0x14-queuing_system_in_js/8-job.js
+++ 0x14-queuing_system_in_js/8-job.js
@@ -3,13 +3,14 @@ const createPushNotificationsJobs = (jobs, queue) => {
 
   jobs.forEach(item => {
     const job = queue.create('push_notification_code_3', item)
-      .save(err => {
-        if (!err) console.log(`Notification job created: ${job.id}`)
-      })
+
     job
       .on("complete", result => console.log(`Notification job ${job.id} completed`))
       .on("failed", errorMessage => console.log(`Notification job ${job.id} failed: ${errorMessage}`))
       .on("progress", (progress, data) => console.log(`Notification job ${job.id} ${progress}% complete`))
+      .save(err => {
+        if (!err) console.log(`Notification job created: ${job.id}`)
+      })
   })
 }
 
