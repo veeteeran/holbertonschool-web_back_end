98484a1ce2ec5d9ea6cb02ba400519d066d0256f
Viet <vietnamtran@gmail.com>
2021-05-30 16:30:51 -0500

    Add routes and tests

--- 0x13-unittests_in_js/10-api/api.test.js
+++ 0x13-unittests_in_js/10-api/api.test.js
@@ -18,3 +18,101 @@ describe('Home route', () => {
     });
   });
 });
+
+describe('Cart route', () => {
+  const goodRoute = 'http://localhost:7865/cart/1'
+  const badRoute = 'http://localhost:7865/cart/hello'
+
+  it('should return status 200', done => {
+    request(goodRoute, (err, res, body) => {
+      (res.statusCode).should.equal(200);
+      done();
+    });
+  });
+
+  it('should return Payment methods for cart 1', done => {
+    request(goodRoute, (err, res, body) => {
+      (body).should.equal('Payment methods for cart 1');
+      done();
+    });
+  });
+
+  it('should return status 404', done => {
+    request(badRoute, (err, res, body) => {
+      (res.statusCode).should.equal(404);
+      done();
+    });
+  });
+
+  it('should return Not a number', done => {
+    request(badRoute, (err, res, body) => {
+      (body).should.equal('Not a number');
+      done();
+    });
+  });
+});
+
+describe('login route', done => {
+  const goodLogin = {
+    url: 'http://localhost:7865/login',
+    method: 'POST',
+    json: { 'userName': 'Betty' }
+  }
+  const badLogin = {
+    url: 'http://localhost:7865/login',
+    method: 'POST',
+    json: { 'user': 'Betty' }
+  }
+
+  it('should return status 200', done => {
+    request.post(goodLogin, (err, res, body) => {
+      (res.statusCode).should.equal(200);
+      done();
+    });
+  });
+
+  it('should return Welcome Betty', done => {
+    request.post(goodLogin, (err, res, body) => {
+      (body).should.equal('Welcome Betty');
+      done();
+    });
+  });
+
+  it('should return status 404', done => {
+    request.post(badLogin, (err, res, body) => {
+      (res.statusCode).should.equal(404);
+      done();
+    });
+  });
+
+  it('should return status 200', done => {
+    request.post(goodLogin, (err, res, body) => {
+      (res.statusCode).should.equal(200);
+      done();
+    });
+  });
+})
+
+describe('available_payments route', done => {
+  const ap = 'http://localhost:7865/available_payments'
+  const obj = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  }
+
+  it('should return status 200', done => {
+    request(ap, (err, res, body) => {
+      (res.statusCode).should.equal(200);
+      done();
+    });
+  });
+
+  it('should deep equal obj', done => {
+    request(ap, (err, res, body) => {
+      (body).should.equal(JSON.stringify(obj));
+      done();
+    });
+  });
+})
