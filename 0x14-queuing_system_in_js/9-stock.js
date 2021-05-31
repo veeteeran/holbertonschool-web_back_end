42ed9a3925e3ac5563ced915d56bc2c84b6f9019
veeteeran <vietnamtran@gmail.com>
2021-05-31 16:02:22 -0500

    Reorganize file

--- 0x14-queuing_system_in_js/9-stock.js
+++ 0x14-queuing_system_in_js/9-stock.js
@@ -1,11 +1,31 @@
+/* Data */
+const listProducts = [
+  { "itemId": 1, "itemName": "Suitcase 250", "price": 50, "initialAvailableQuantity": 4 },
+  { "itemId": 2, "itemName": "Suitcase 450", "price": 100, "initialAvailableQuantity": 10 },
+  { "itemId": 3, "itemName": "Suitcase 650", "price": 350, "initialAvailableQuantity": 2 },
+  { "itemId": 4, "itemName": "Suitcase 1050", "price": 550, "initialAvailableQuantity": 5 }
+]
+
+/* Data access */
+const getItemById = id => {
+  for (const item of listProducts) {
+    if (item.itemId === id) return item
+  }
+}
+
+/* Server */
 const express = require('express');
 const app = express();
 const port = 1245;
+app.listen(port)
 
+/* Products */
+app.get('/list_products', (req, res) => res.json(listProducts))
+
+/* In stock in Redis */
 const client = require('redis').createClient();
 import { promisify } from 'util'
 const getAsync = promisify(client.get).bind(client)
-const setAsync = promisify(client.set).bind(client)
 
 client.on("connect", () => {
   console.log("Redis client connected to the server")
@@ -15,58 +35,44 @@ client.on("error", error => {
   console.log(`Redis client not connected to the server: ${error}`)
 })
 
-const listProducts = [
-  { "itemId": 1, "itemName": "Suitcase 250", "price": 50, "initialAvailableQuantity": 4 },
-  { "itemId": 2, "itemName": "Suitcase 450", "price": 100, "initialAvailableQuantity": 10 },
-  { "itemId": 3, "itemName": "Suitcase 650", "price": 350, "initialAvailableQuantity": 2 },
-  { "itemId": 4, "itemName": "Suitcase 1050", "price": 550, "initialAvailableQuantity": 5 }
-]
+const reserveStockById = (itemId, stock) => client.set(itemId, stock)
 
-const getItemById = id => {
-  for (const item of listProducts) {
-    if (item.itemId === id) return item
-  }
-}
+const getCurrentReservedStockById = async (itemId) => await getAsync(itemId)
 
-app.get('/list_products', (req, res) => res.json(listProducts))
-
+/* Product detail */
 app.get('/list_products/:itemId', async (req, res) => {
   const { itemId } = req.params
-  const inStock = getItemById(parseInt(itemId))
-  const reserved = await getCurrentReservedStockById(parseInt(itemId))
-  if (inStock) {
-    if (reserved) {
-      inStock.currentQuantity = reserved
+  const item = getItemById(parseInt(itemId))
+  const quantity = await getCurrentReservedStockById(parseInt(itemId))
+
+  if (item) {
+    if (quantity) {
+      item.currentQuantity = quantity
     } else {
-      inStock.currentQuantity = inStock.initialAvailableQuantity
-      await reserveStockById(itemId, inStock.initialAvailableQuantity)
+      item.currentQuantity = item.initialAvailableQuantity
+      reserveStockById(itemId, item.initialAvailableQuantity)
     }
-    res.json(inStock)
+    res.json(item)
   } else {
     res.json({ "status": "Product not found" })
   }
 })
 
+/* Reserve a product */
 app.get('/reserve_product/:itemId', async (req, res) => {
   const { itemId } = req.params
-  const inStock = getItemById(parseInt(itemId))
-  const reserved = await getCurrentReservedStockById(parseInt(itemId))
-  if (!inStock) res.json({ "status": "Product not found" })
+  const item = getItemById(parseInt(itemId))
+  const quantity = await getCurrentReservedStockById(parseInt(itemId))
 
-  if (!reserved || reserved > 0) {
-    inStock.initialAvailableQuantity -= 1
-    inStock.currentQuantity = inStock.initialAvailableQuantity
-    await reserveStockById(itemId, inStock.initialAvailableQuantity)
+  if (!item) res.json({ "status": "Product not found" })
+
+  if (!quantity) {
+    reserveStockById(itemId, item.initialAvailableQuantity - 1)
     res.json({ "status": "Reservation confirmed", "itemId": `${itemId}` })
-  } else if (reserved < 1) {
+  } else if (quantity > 0) {
+    reserveStockById(itemId, quantity - 1)
+    res.json({ "status": "Reservation confirmed", "itemId": `${itemId}` })
+  } else if (quantity < 1) {
     res.json({ "status": "Not enough stock available", "itemId": `${itemId}` })
   }
 })
-
-app.listen(port)
-
-const reserveStockById = async (itemId, stock) => await setAsync(itemId, stock)
-
-const getCurrentReservedStockById = async (itemId) => await getAsync(itemId)
-
-module.exports = app;
