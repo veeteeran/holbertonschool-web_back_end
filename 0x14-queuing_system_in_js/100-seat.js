03a8272d98af1975b7c476398a5cd95610ea2c1f
veeteeran <vietnamtran@gmail.com>
2021-06-08 07:27:09 -0500

    Eslint

--- 0x14-queuing_system_in_js/100-seat.js
+++ 0x14-queuing_system_in_js/100-seat.js
@@ -1,76 +1,81 @@
+/* eslint-disable no-unused-expressions */
 /* Create client */
-const client = require("redis").createClient()
-import { promisify } from 'util'
-const getAsync = promisify(client.get).bind(client)
+import { promisify } from 'util';
 
-client.on("connect", () => {
-  console.log("Redis client connected to the server")
-  reserveSeat(50)
-})
+const client = require('redis').createClient();
 
-client.on("error", error => {
-  console.log(`Redis client not connected to the server: ${error}`)
-})
+const getAsync = promisify(client.get).bind(client);
 
-const reserveSeat = number => client.set("available_seats", number)
+const reserveSeat = (number) => client.set('available_seats', number);
 
-const getCurrentAvailableSeats = async () => await getAsync("available_seats")
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  reserveSeat(50);
+});
 
-let reservationEnabled = true
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+const getCurrentAvailableSeats = async () => getAsync('available_seats');
+
+let reservationEnabled = true;
 
 /* Create queue */
-const queue = require("kue").createQueue()
+const queue = require('kue').createQueue();
 
 /* Create server */
-const express = require("express")
-const app = express()
-const port = 1245
+const express = require('express');
+
+const app = express();
+const port = 1245;
 
 app.get('/available_seats', async (req, res) => {
-  const availableSeats = await getCurrentAvailableSeats()
-  res.json({ numberOfAvailableSeats: availableSeats })
-})
+  const availableSeats = await getCurrentAvailableSeats();
+  res.json({ numberOfAvailableSeats: availableSeats });
+});
 
 app.get('/reserve_seat', (req, res) => {
   if (reservationEnabled) {
     const job = queue.create('reserve_seat')
-      .save(err => {
-        err ?
-          res.json({ "status": "Reservation failed" }) :
-          res.json({ "status": "Reservation in process" })
-      })
+      .save((err) => {
+        err
+          ? res.json({ status: 'Reservation failed' })
+          : res.json({ status: 'Reservation in process' });
+      });
 
     job
-      .on("complete", result => {
-        console.log(`Seat reservation job ${job.id} completed`)
+      .on('complete', () => {
+        console.log(`Seat reservation job ${job.id} completed`);
       })
-      .on("failed", errorMessage => {
-        console.log(`Seat reservation job ${job.id} failed: ${errorMessage}`)
-      })
+      .on('failed', (errorMessage) => {
+        console.log(`Seat reservation job ${job.id} failed: ${errorMessage}`);
+      });
   } else {
-    res.json({ "status": "Reservation are blocked" })
+    res.json({ status: 'Reservation are blocked' });
   }
-})
+});
 
 app.get('/process', (req, res) => {
-  queue.process("reserve_seat", async (job, done) => {
+  queue.process('reserve_seat', async (job, done) => {
     try {
-      const availableSeats = await getCurrentAvailableSeats()
+      const availableSeats = await getCurrentAvailableSeats();
       if (availableSeats > 0) {
-        await reserveSeat(availableSeats - 1)
-        if ((availableSeats - 1) === 0) reservationEnabled = false
+        await reserveSeat(availableSeats - 1);
+        if ((availableSeats - 1) === 0) reservationEnabled = false;
       }
 
       if ((availableSeats - 1) >= 0) {
-        done()
+        done();
       } else {
-        return done(Error("Not enough seats available"))
+        return done(Error('Not enough seats available'));
       }
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
-  })
-  res.json({ "status": "Queue processing" })
-})
+    return null;
+  });
+  res.json({ status: 'Queue processing' });
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
--- 0x14-queuing_system_in_js/8-job.test.js
+++ 0x14-queuing_system_in_js/8-job.test.js
@@ -1,36 +1,45 @@
-import createPushNotificationsJobs from './8-job'
-import kue from 'kue'
-const queue = kue.createQueue()
-const { testMode: { clear, enter, exit, jobs } } = queue
-import { expect } from "chai"
+/* eslint-disable jest/valid-expect */
+/* eslint-disable no-undef */
+/* eslint-disable jest/no-hooks */
+/* eslint-disable jest/prefer-expect-assertions */
+import kue from 'kue';
+import { expect } from 'chai';
+import createPushNotificationsJobs from './8-job';
+
+const queue = kue.createQueue();
+const {
+  testMode: {
+    clear, enter, exit, jobs,
+  },
+} = queue;
 
 describe('#createPushNotificationsJobs()', () => {
-  before(() => enter())
-  afterEach(() => clear())
-  after(() => exit())
+  before(() => enter());
+  afterEach(() => clear());
+  after(() => exit());
 
   it('should throw "Jobs is not an array" when jobs is not an Array', () => {
-    const str = "Not an array"
-    const badFn = () => createPushNotificationsJobs(str, queue)
-    expect(badFn).to.throw(Error, "Jobs is not an array")
-  })
+    const str = 'Not an array';
+    const badFn = () => createPushNotificationsJobs(str, queue);
+    expect(badFn).to.throw(Error, 'Jobs is not an array');
+  });
 
   it('should create two new jobs to the queue', () => {
     const list = [
       {
         phoneNumber: '4153518780',
-        message: 'This is the code 1234 to verify your account'
+        message: 'This is the code 1234 to verify your account',
       },
       {
         phoneNumber: '4153518781',
-        message: 'This is the code 4562 to verify your account'
-      }
-    ]
-    createPushNotificationsJobs(list, queue)
-    expect(jobs.length).to.equal(2)
-    expect(jobs[0].type).to.equal('push_notification_code_3')
+        message: 'This is the code 4562 to verify your account',
+      },
+    ];
+    createPushNotificationsJobs(list, queue);
+    expect(jobs.length).to.equal(2);
+    expect(jobs[0].type).to.equal('push_notification_code_3');
     expect(queue.testMode.jobs[0].data).to.eql(list[0]);
-    expect(jobs[1].type).to.equal('push_notification_code_3')
+    expect(jobs[1].type).to.equal('push_notification_code_3');
     expect(queue.testMode.jobs[1].data).to.eql(list[1]);
-  })
-})
+  });
+});
